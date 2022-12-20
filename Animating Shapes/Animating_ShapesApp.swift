1cf88d047031901ae91af819f45d599edc00e221
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:48:01 -0300

    Initial Commit

---
+++ Animating Shapes/Animating_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_ShapesApp.swift
+//  Animating Shapes
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Animating_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
